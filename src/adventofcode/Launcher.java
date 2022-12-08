67dc43a36dd00f9b5dc930b83ade2798592df210
Julien Herment <julien.herment.ext@geodis.com>
2022-12-08 15:01:58 +0100

    Update day 3 of 2021

--- src/adventofcode/Launcher.java
+++ src/adventofcode/Launcher.java
@@ -5,8 +5,8 @@ import utilities.AOCFactory;
 import utilities.Printer;
 
 public class Launcher {
-    private static final int YEAR = 2022;
-    private static final int DAY = 8;
+    private static final int YEAR = 2021;
+    private static final int DAY = 3;
     public static void main(String[] args) {
     	AOC aoc = AOCFactory.getAOC(YEAR, DAY);
         Printer.println("Solution " + YEAR + "/12/" + DAY);
--- src/adventofcode/aoc_2021_03/AOC_2021_03.java
+++ src/adventofcode/aoc_2021_03/AOC_2021_03.java
@@ -7,6 +7,8 @@ import utilities.Printer;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AOC_2021_03 extends AOC {
     @Override
@@ -14,12 +16,12 @@ public class AOC_2021_03 extends AOC {
         try {
             List<String> list = FileLoader.readListFromFile(file);
             List<Integer> binaryAverage = initList(list.get(0).length(), 0);
-            extractBinary(list, binaryAverage);
+            extractBinaryCount(list, binaryAverage);
 
             StringBuilder stringBuilder1 = new StringBuilder();
             StringBuilder stringBuilder2 = new StringBuilder();
-            for(Integer integer : binaryAverage) {
-                if(integer > list.size()/2) {
+            for (Integer integer : binaryAverage) {
+                if (integer > list.size() / 2) {
                     stringBuilder1.append("1");
                     stringBuilder2.append("0");
                 } else {
@@ -27,8 +29,20 @@ public class AOC_2021_03 extends AOC {
                     stringBuilder2.append("1");
                 }
             }
+            int gammaRate = Integer.parseInt(stringBuilder1.toString(), 2);
+            int epsilonRate = Integer.parseInt(stringBuilder2.toString(), 2);
 
-            Printer.println(Integer.parseInt(stringBuilder1.toString(), 2) * Integer.parseInt(stringBuilder2.toString(), 2));
+            Printer.println("Solution 1 : " + (gammaRate * epsilonRate));
+
+            String binaryO2Rate = extractBinaryCountFiltered(list, 0,  true);
+            String binaryCO2Rate = extractBinaryCountFiltered(list, 0, false);
+
+            int CO2Rate = Integer.parseInt(binaryCO2Rate, 2);
+            int O2Rate = Integer.parseInt(binaryO2Rate, 2);
+
+            Printer.println(binaryO2Rate + " " + binaryCO2Rate);
+
+            Printer.println("Solution 2 : " + (O2Rate * CO2Rate));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -37,21 +51,40 @@ public class AOC_2021_03 extends AOC {
 
     private List<Integer> initList(int size, int defaultVal) {
         List<Integer> binaryAverage = new ArrayList<>();
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             binaryAverage.add(defaultVal);
         }
         return binaryAverage;
     }
 
-    private void extractBinary(List<String> list, List<Integer> binaryAverage) {
-        for(String string : list) {
+    private void extractBinaryCount(List<String> list, List<Integer> binaryAverage) {
+        for (String item : list) {
             int incr = 0;
-            for(char c : string.toCharArray()) {
-                if(c == '0') {
-                    binaryAverage.set(incr, binaryAverage.get(incr) + 1);
-                }
+            for (char c : item.toCharArray()) {
+                extractBinary(c, binaryAverage, incr);
                 incr++;
             }
         }
+    }
+
+    private void extractBinary(char item, List<Integer> binaryAverage, int position) {
+        if (item == '0') {
+            binaryAverage.set(position, binaryAverage.get(position) + 1);
+        }
+    }
+
+    private String extractBinaryCountFiltered(List<String> list, int index, boolean isO2) {
+        int finalIndex = index;
+        long count1 = list.stream().filter(e -> e.charAt(finalIndex) == '1').count();
+        long count2 = list.size() - count1;
+        char nextChar;
+        if (count1 >= count2) {
+            nextChar = (isO2) ? '1' : '0';
+        } else {
+            nextChar = (isO2) ? '0' : '1';
+        }
+        index++;
+        List<String> filteredList = list.stream().filter(e -> e.charAt(finalIndex) == nextChar).collect(Collectors.toList());
+        return  filteredList.size() > 1 ? extractBinaryCountFiltered(filteredList, index, isO2) : filteredList.get(0);
     }
 }
