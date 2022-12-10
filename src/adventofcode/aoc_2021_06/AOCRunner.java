397f9f6fd916f6fe3854ad45573ae73150c7a4ac
JHERMENT <avr1l.dunmare@gmail.com>
2022-12-10 10:13:31 +0100

    Add day 6 (year 2021)

---
+++ src/adventofcode/aoc_2021_06/AOCRunner.java
@@ -0,0 +1,47 @@
+package adventofcode.aoc_2021_06;
+
+import template.AOC;
+import utilities.FileLoader;
+import utilities.Printer;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AOCRunner implements AOC {
+    private Map<Integer, Long> fishes = new HashMap<>();
+    private List<Long> newbornFishes = new ArrayList<>();
+
+    @Override
+    public void run(String file) {
+        try {
+            int days1 = 80;
+            int days2 = 256;
+            List<String> list = FileLoader.readListFromFile(file);
+            for (String item : list) {
+                Arrays.stream(item.split(",")).map(Integer::parseInt).forEach(fishDay -> fishes.compute(fishDay, (k, v) -> (v != null) ? v + 1 : 1));
+
+                for (int day = 0; day < days1; day++) {
+                    live(day);
+                }
+                Printer.println("Solution 1 : " + fishes.values().stream().reduce(Long::sum).orElse(0l));
+
+                for (int day = days1; day < days2; day++) {
+                    live(day);
+                }
+                Printer.println("Solution 2 : " + fishes.values().stream().reduce(Long::sum).orElse(0l));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void live(int day) {
+        int dayOfWeek = day % 7;
+        long babiesCantProduct = (day - 2 > 0) ? newbornFishes.get(day - 2) : 0;
+        long newFishes = (fishes.get(dayOfWeek) != null) ? fishes.get(dayOfWeek) - babiesCantProduct : 0;
+        newbornFishes.add(newFishes);
+        fishes.compute((day + 2) % 7, (k, v) -> (v != null) ? v + newFishes : newFishes);
+    }
+
+}
