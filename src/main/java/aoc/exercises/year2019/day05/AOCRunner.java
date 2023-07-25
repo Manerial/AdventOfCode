c0a62239790bb32b760aa11b48965221be24bc88
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:41:07 +0200

    Add exercise 05 of 2019

--- src/main/java/aoc/exercises/year2019/day05/AOCRunner.java
+++ src/main/java/aoc/exercises/year2019/day05/AOCRunner.java
@@ -2,6 +2,9 @@ package aoc.exercises.year2019.day05;
 
 import utilities.AbstractAOC;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2019/day/5">here</a>
@@ -11,7 +14,17 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> memory = Stream.of(inputList.get(0).split(","))
+                .map(Integer::parseInt)
+                .toList();
+        OpCodeComputer opCodeComputer = new OpCodeComputer(memory);
+
+        opCodeComputer.setComputerInput(1);
+        opCodeComputer.compute();
+        solution1 = opCodeComputer.getComputerOutput();
+
+        opCodeComputer.setComputerInput(5);
+        opCodeComputer.compute();
+        solution2 = opCodeComputer.getComputerOutput();
     }
 }
