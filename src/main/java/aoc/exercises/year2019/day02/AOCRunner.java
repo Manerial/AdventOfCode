a16b9292319720b7b1fc0f71d02b9d93e3e1183c
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:39:34 +0200

    Add exercise 02 of 2019

--- src/main/java/aoc/exercises/year2019/day02/AOCRunner.java
+++ src/main/java/aoc/exercises/year2019/day02/AOCRunner.java
@@ -2,16 +2,32 @@ package aoc.exercises.year2019.day02;
 
 import utilities.AbstractAOC;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2019/day/2">here</a>
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    public static final int AWAITED_OUTPUT = 19690720;
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> memory = Stream.of(inputList.get(0).split(","))
+                .map(Integer::parseInt)
+                .toList();
+
+        OpCodeComputer opCodeComputer = new OpCodeComputer(memory);
+        if(isExample) {
+            opCodeComputer.compute(9, 10);
+            solution1 = opCodeComputer.getComputerOutput();
+            solution2 = 1202; // Cannot be tested
+        } else {
+            opCodeComputer.compute(12, 2);
+            solution1 = opCodeComputer.getComputerOutput();
+            solution2 = opCodeComputer.computeUntilAwaited(AWAITED_OUTPUT);
+        }
     }
 }
