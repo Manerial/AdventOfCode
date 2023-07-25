13f7d29b9fa6550586ad0d23e3fd20d7f142eb27
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:31:19 +0200

    Add exercise 03 of 2015

---
+++ src/main/java/aoc/exercises/year2015/day03/Santa.java
@@ -0,0 +1,13 @@
+package aoc.exercises.year2015.day03;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import aoc.common_objects.Position;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Santa {
+    private Position position;
+}
