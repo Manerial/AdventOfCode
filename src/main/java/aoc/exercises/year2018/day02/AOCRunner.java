a7d315f362094fea92422b652d31b768814ff1c1
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:56:52 +0200

    Add exercise 02 of 2018

--- src/main/java/aoc/exercises/year2018/day02/AOCRunner.java
+++ src/main/java/aoc/exercises/year2018/day02/AOCRunner.java
@@ -2,6 +2,8 @@ package aoc.exercises.year2018.day02;
 
 import utilities.AbstractAOC;
 
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2018/day/2">here</a>
@@ -11,7 +13,49 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        int countDouble = 0;
+        int countTriple = 0;
+        for (String item : inputList) {
+            countDouble += hasAnyLetterTimes(item, 2);
+            countTriple += hasAnyLetterTimes(item, 3);
+        }
+
+        solution1 = countDouble * countTriple;
+
+        solution2 = getSimilarLine(inputList);
+    }
+
+    private int hasAnyLetterTimes(String item, int times) {
+        for (char c : item.toCharArray()) {
+            long count = item.chars().filter(value -> value == c).count();
+            if (count == times) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    private String getSimilarLine(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                String string1 = list.get(i);
+                String string2 = list.get(j);
+                String similar = getSimilar(string1.toCharArray(), string2.toCharArray());
+                if (similar.length() == string1.length() - 1) {
+                    return similar;
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getSimilar(char[] chars1, char[] chars2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars1.length; i++) {
+            if (chars1[i] == chars2[i]) {
+                sb.append(chars1[i]);
+            }
+        }
+        return sb.toString();
     }
 }
--- src/test/java/aoc/exercises/year2018/day02/AOCRunnerTest.java
+++ src/test/java/aoc/exercises/year2018/day02/AOCRunnerTest.java
@@ -16,15 +16,15 @@ class AOCRunnerTest {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), true);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(2), aocRunner.getSolution1().toString()),
+                () -> assertEquals("fgijk", aocRunner.getSolution2().toString()));
     }
     @Test
     void test_puzzle() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), false);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(8715), aocRunner.getSolution1().toString()),
+                () -> assertEquals("fvstwblgqkhpuixdrnevmaycd", aocRunner.getSolution2().toString()));
     }
 }
