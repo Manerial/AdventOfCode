fcc7a2f47c7cdc64972cd6736da6ff1120fe6b5d
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:19:34 +0200

    Add exercise 01 of 2020

--- src/main/java/aoc/exercises/year2020/day01/AOCRunner.java
+++ src/main/java/aoc/exercises/year2020/day01/AOCRunner.java
@@ -2,6 +2,9 @@ package aoc.exercises.year2020.day01;
 
 import utilities.AbstractAOC;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2020/day/1">here</a>
@@ -11,7 +14,32 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> list = inputList.stream()
+                .map(Integer::parseInt)
+                .sorted()
+                .toList();
+
+        solution1 = recursiveFunction(list, 2020, 2);
+        solution2 = recursiveFunction(list, 2020, 3);
+    }
+
+    private int recursiveFunction(List<Integer> list, int target, int nbElements) {
+        if (nbElements == 1) {
+            if (Collections.binarySearch(list, target) >= 0) {
+                return target;
+            }
+            return -1;
+        }
+
+        for(int index = 0; index < list.size(); index++) {
+            int currentNumber = list.get(index);
+            int diff = target - currentNumber;
+            List<Integer> subList = list.subList(index + 1, list.size());
+            int recursiveResult = recursiveFunction(subList, diff, nbElements - 1);
+            if(recursiveResult >= 0) {
+                return recursiveResult * currentNumber;
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/aoc/exercises/year2020/day01/AOCRunnerTest.java
+++ src/test/java/aoc/exercises/year2020/day01/AOCRunnerTest.java
@@ -16,15 +16,15 @@ class AOCRunnerTest {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), true);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(514579), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(241861950), aocRunner.getSolution2().toString()));
     }
     @Test
     void test_puzzle() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), false);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(802011), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(248607374), aocRunner.getSolution2().toString()));
     }
 }
