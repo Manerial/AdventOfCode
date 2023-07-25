d346338be0d5b45c23d86ca556bbe1c83b1dda17
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:57:12 +0200

    Add exercise 03 of 2018

--- src/test/java/aoc/exercises/year2018/day03/AOCRunnerTest.java
+++ src/test/java/aoc/exercises/year2018/day03/AOCRunnerTest.java
@@ -16,15 +16,16 @@ class AOCRunnerTest {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), true);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(4), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(3), aocRunner.getSolution2().toString()));
     }
     @Test
     void test_puzzle() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), false);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(104712), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(840), aocRunner.getSolution2().toString()));
     }
+
 }
