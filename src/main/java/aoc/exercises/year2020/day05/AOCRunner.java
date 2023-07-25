04f35e30468f5bf169535428140fe3298175cf87
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:43:39 +0200

    Add exercise 05 of 2020

--- src/main/java/aoc/exercises/year2020/day05/AOCRunner.java
+++ src/main/java/aoc/exercises/year2020/day05/AOCRunner.java
@@ -2,6 +2,10 @@ package aoc.exercises.year2020.day05;
 
 import utilities.AbstractAOC;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2020/day/5">here</a>
@@ -11,7 +15,22 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> seatIds = new ArrayList<>(parseInput());
+
+        solution1 = Collections.max(seatIds);
+
+        int nbOfSeats = seatIds.size() + 1;
+        int firstSeatId = Collections.min(seatIds);
+        int sumOfAllSeatsId = ((nbOfSeats) * (nbOfSeats - 1)) / 2 + (firstSeatId * nbOfSeats);
+        int sumOfAssignedSeatsId = seatIds.stream().mapToInt(Integer::intValue).sum();
+
+        solution2 = sumOfAllSeatsId - sumOfAssignedSeatsId;
+    }
+
+    private List<Integer> parseInput() {
+        return inputList.stream()
+                .map(PlanesSeat::new)
+                .map(PlanesSeat::getSeat)
+                .toList();
     }
 }
--- src/test/java/aoc/exercises/year2020/day05/AOCRunnerTest.java
+++ src/test/java/aoc/exercises/year2020/day05/AOCRunnerTest.java
@@ -1,5 +1,6 @@
 package aoc.exercises.year2020.day05;
 
+
 import aoc.AOCRunnerTestBuilder;
 import org.junit.jupiter.api.Test;
 import utilities.AbstractAOC;
@@ -11,20 +12,21 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AOCRunnerTest {
+
     @Test
     void test_example() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), true);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(820), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(-1258), aocRunner.getSolution2().toString()));
     }
     @Test
     void test_puzzle() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), false);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(818), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(559), aocRunner.getSolution2().toString()));
     }
 }
