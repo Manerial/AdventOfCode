ee5e7021ccef398d82bfe2938b57e3bd77ec0efc
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:19:46 +0200

    Add exercise 01 of 2021

--- src/main/java/aoc/exercises/year2021/day01/AOCRunner.java
+++ src/main/java/aoc/exercises/year2021/day01/AOCRunner.java
@@ -2,6 +2,9 @@ package aoc.exercises.year2021.day01;
 
 import utilities.AbstractAOC;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2021/day/1">here</a>
@@ -11,7 +14,25 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        solution1 = countGreaterMeasure(inputList, 1);
+        solution2 = countGreaterMeasure(inputList, 3);
+    }
+
+    private int countGreaterMeasure(List<String> list, int glide) {
+        int count = 0;
+        int last = 0;
+        List<Integer> glider = new ArrayList<>();
+        for (String item : list) {
+            glider.add(Integer.parseInt(item));
+            if (glider.size() > glide) {
+                glider.remove(0);
+                int current = glider.stream().reduce(0, Integer::sum);
+                if (last < current) {
+                    count++;
+                }
+                last = current;
+            }
+        }
+        return count;
     }
 }
