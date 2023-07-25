b0b7cca9583494512eacdb099981b5480af056d1
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:46:48 +0200

    Add exercise 06 of 2021

--- src/main/java/aoc/exercises/year2021/day06/AOCRunner.java
+++ src/main/java/aoc/exercises/year2021/day06/AOCRunner.java
@@ -2,16 +2,43 @@ package aoc.exercises.year2021.day06;
 
 import utilities.AbstractAOC;
 
+import java.util.*;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2021/day/6">here</a>
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    private final Map<Integer, Long> fishes = new HashMap<>();
+    private final List<Long> newbornFishes = new ArrayList<>();
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        int days1 = 80;
+        int days2 = 256;
+
+        for (String item : inputList) {
+            Arrays.stream(item.split(",")).map(Integer::parseInt).forEach(fishDay -> fishes.compute(fishDay, (k, v) -> (v != null) ? v + 1 : 1));
+
+            for (int day = 0; day < days1; day++) {
+                live(day);
+            }
+            solution1 = fishes.values().stream().reduce(Long::sum).orElse(0L);
+
+            for (int day = days1; day < days2; day++) {
+                live(day);
+            }
+            solution2 = fishes.values().stream().reduce(Long::sum).orElse(0L);
+        }
     }
+
+    private void live(int day) {
+        int dayOfWeek = day % 7;
+        long babiesCantProduct = (day - 2 > 0) ? newbornFishes.get(day - 2) : 0;
+        long newFishes = (fishes.get(dayOfWeek) != null) ? fishes.get(dayOfWeek) - babiesCantProduct : 0;
+        newbornFishes.add(newFishes);
+        fishes.compute((day + 2) % 7, (k, v) -> (v != null) ? v + newFishes : newFishes);
+    }
+
 }
--- src/test/java/aoc/exercises/year2021/day06/AOCRunnerTest.java
+++ src/test/java/aoc/exercises/year2021/day06/AOCRunnerTest.java
@@ -11,20 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AOCRunnerTest {
+
     @Test
     void test_example() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), true);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(5934), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(26984457539L), aocRunner.getSolution2().toString()));
     }
     @Test
     void test_puzzle() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), false);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(386755), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(1732731810807L), aocRunner.getSolution2().toString()));
     }
 }
