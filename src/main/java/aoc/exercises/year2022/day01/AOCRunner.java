257be08fce6c6c8a61ca9fe714098b383a918318
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:20:02 +0200

    Add exercise 01 of 2022

--- src/main/java/aoc/exercises/year2022/day01/AOCRunner.java
+++ src/main/java/aoc/exercises/year2022/day01/AOCRunner.java
@@ -1,6 +1,13 @@
 package aoc.exercises.year2022.day01;
 
+import aoc.common_objects.SortedLimitedList;
 import utilities.AbstractAOC;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static utilities.ResourceIO.DELIMITER;
 
 /**
  * <pre>
@@ -11,7 +18,32 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> backpacks = createBackpacks(inputList).stream()
+                .toList();
+        solution1 = backpacks.getFirst();
+        solution2 = backpacks.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private List<Integer> createBackpacks(List<String> list) {
+        String baseInput = String.join(DELIMITER, list);
+        String[] backpacksStr = baseInput.split(DELIMITER + DELIMITER);
+
+        List<Integer> backpacks = new SortedLimitedList<>(Collections.reverseOrder(), 3);
+        backpacks.addAll(getBackpacks(backpacksStr));
+        return backpacks;
+    }
+
+    private List<Integer> getBackpacks(String[] backpacksStr) {
+        return Stream.of(backpacksStr)
+                .map(this::getBackpack)
+                .toList();
+    }
+
+    private int getBackpack(String backpack) {
+        return Stream.of(backpack.split(DELIMITER))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
