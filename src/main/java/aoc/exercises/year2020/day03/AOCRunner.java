62fd91a64df6da6e7d2d5d1f5839e65fa4b6b8aa
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:42:48 +0200

    Add exercise 03 of 2020

--- src/main/java/aoc/exercises/year2020/day03/AOCRunner.java
+++ src/main/java/aoc/exercises/year2020/day03/AOCRunner.java
@@ -1,7 +1,9 @@
-
 package aoc.exercises.year2020.day03;
 
 import utilities.AbstractAOC;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <pre>
@@ -12,7 +14,30 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> listTreesOnTheWay = new ArrayList<>();
+        listTreesOnTheWay.add(countTreesOnWay(1, 1));
+        listTreesOnTheWay.add(countTreesOnWay(1, 3));
+        listTreesOnTheWay.add(countTreesOnWay(1, 5));
+        listTreesOnTheWay.add(countTreesOnWay(1, 7));
+        listTreesOnTheWay.add(countTreesOnWay(2, 1));
+
+        solution1 = listTreesOnTheWay.get(1);
+        solution2 = listTreesOnTheWay.stream()
+                .mapToLong(Integer::longValue)
+                .reduce(1L, Math::multiplyExact);
+    }
+
+    public int countTreesOnWay(int down, int right) {
+        int charIndex = 0;
+        int trees = 0;
+
+        for (int currentLineIndex = 0; currentLineIndex < inputList.size(); currentLineIndex += down) {
+            String inputLine = inputList.get(currentLineIndex);
+            char currentChar = inputLine.charAt(charIndex % inputLine.length());
+            trees += (currentChar == '#') ? 1 : 0;
+            charIndex += right;
+        }
+
+        return trees;
     }
 }
