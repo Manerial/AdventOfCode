24da8dd3ceb7435fefbb0979354afbc60b087034
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:54:33 +0200

    Add exercise 05 of 2017

--- src/main/java/aoc/exercises/year2017/day05/AOCRunner.java
+++ src/main/java/aoc/exercises/year2017/day05/AOCRunner.java
@@ -2,6 +2,9 @@ package aoc.exercises.year2017.day05;
 
 import utilities.AbstractAOC;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2017/day/5">here</a>
@@ -11,7 +14,33 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> instructions = initList();
+        solution1 = mazeSolve(instructions.toArray(new Integer[0]), false);
+        solution2 = mazeSolve(instructions.toArray(new Integer[0]), true);
+    }
+
+    private List<Integer> initList() {
+        List<Integer> instructions = new ArrayList<>();
+        for (String line : inputList) {
+            int instruction = Integer.parseInt(line);
+            instructions.add(instruction);
+        }
+        return instructions;
+    }
+
+    private int mazeSolve(Integer[] instructions, boolean crazySolving) {
+        int index = 0;
+        int steps = 0;
+        while (index < instructions.length) {
+            int instruction = instructions[index];
+            if (crazySolving && instruction >= 3) {
+                instructions[index]--;
+            } else {
+                instructions[index]++;
+            }
+            index += instruction;
+            steps++;
+        }
+        return steps;
     }
 }
