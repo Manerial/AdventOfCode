593a5b5bf7f39b8fffa2e2f3ba38cf384cda74ba
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:55:08 +0200

    Add exercise 06 of 2017

--- src/main/java/aoc/exercises/year2017/day06/AOCRunner.java
+++ src/main/java/aoc/exercises/year2017/day06/AOCRunner.java
@@ -2,16 +2,57 @@ package aoc.exercises.year2017.day06;
 
 import utilities.AbstractAOC;
 
+import java.util.*;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2017/day/6">here</a>
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    private final Set<String> banksSet = new LinkedHashSet<>();
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        int[] duplicatedLoop = dispatchLoop();
+
+        solution1 = banksSet.size();
+
+        List<String> banksList = new ArrayList<>(this.banksSet);
+        solution2 = banksList.size() - banksList.indexOf(Arrays.toString(duplicatedLoop));
+    }
+
+    private int[] getBanks() {
+        return Arrays.stream(inputList.get(0).split("\t"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private int[] dispatchLoop() {
+        int[] lastBanks = getBanks();
+
+        while (banksSet.add(Arrays.toString(lastBanks))) {
+            int[] newBanks = Arrays.copyOf(lastBanks, lastBanks.length);
+            int currentMax = Arrays.stream(lastBanks).max().orElseThrow();
+
+            for(int index = 0; index < newBanks.length; index++) {
+                if(newBanks[index] == currentMax) {
+                    dispatch(currentMax, newBanks, index);
+                    break;
+                }
+            }
+            lastBanks = newBanks;
+
+        }
+        return lastBanks;
+    }
+
+    private static void dispatch(int currentMax, int[] newBanks, int index) {
+        int currentIndex = index;
+        newBanks[currentIndex] = 0;
+        for(int decrement = currentMax; decrement > 0; decrement--) {
+            currentIndex++;
+            newBanks[currentIndex%newBanks.length] += 1;
+        }
     }
 }
