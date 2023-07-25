fce8ea8043aca473c92efba49cc84f5cb7b36cc2
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:52:21 +0200

    Add exercise 02 of 2017

--- src/main/java/aoc/exercises/year2017/day02/AOCRunner.java
+++ src/main/java/aoc/exercises/year2017/day02/AOCRunner.java
@@ -1,6 +1,11 @@
 package aoc.exercises.year2017.day02;
 
 import utilities.AbstractAOC;
+import utilities.errors.NoSuchElementInListException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * <pre>
@@ -11,7 +16,45 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        int sumMinMax = 0;
+        int sumDivisions = 0;
+        for (String line : inputList) {
+            List<Integer> numbers = Arrays.stream(line.split("\\s+"))
+                    .map(Integer::parseInt)
+                    .toList();
+            sumMinMax += sumMinMax(numbers);
+            sumDivisions += sumDivisions(numbers);
+        }
+        solution1 = sumMinMax;
+
+        solution2 = sumDivisions;
+    }
+
+    private static int sumMinMax(List<Integer> list) {
+        int min = Collections.min(list);
+        int max = Collections.max(list);
+        return max - min;
+    }
+
+    private int sumDivisions(List<Integer> numbers) {
+        for (Integer currentNumber : numbers) {
+            int sum = numbers.stream()
+                    .filter(number -> isDivisible(currentNumber, number))
+                    .filter(number -> !currentNumber.equals(number))
+                    .mapToInt(number -> getDivision(currentNumber, number))
+                    .sum();
+            if(sum > 0) {
+                return sum;
+            }
+        }
+        throw new NoSuchElementInListException();
+    }
+
+    private static int getDivision(int num1, int num2) {
+        return Math.max(num1, num2) / Math.min(num1, num2);
+    }
+
+    private static boolean isDivisible(int num1, int num2) {
+        return num1 % num2 == 0 || num2 % num1 == 0;
     }
 }
