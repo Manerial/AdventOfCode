8f1b4d07d13c3a625add95ab26de3e7dbbd48b51
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:48:40 +0200

    Add exercise 04 of 2022

--- src/main/java/aoc/exercises/year2022/day04/AOCRunner.java
+++ src/main/java/aoc/exercises/year2022/day04/AOCRunner.java
@@ -1,6 +1,7 @@
 package aoc.exercises.year2022.day04;
 
 import utilities.AbstractAOC;
+import aoc.common_objects.Range;
 
 /**
  * <pre>
@@ -11,7 +12,29 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        int contains = 0;
+        int overlap = 0;
+        for (String string : inputList) {
+            // We get our ranges
+            Range range1 = getRange(string.split(",")[0]);
+            Range range2 = getRange(string.split(",")[1]);
+            // If one contains the other, ++
+            if (range1.contains(range2) || range2.contains(range1)) {
+                contains++;
+            }
+            // If one overlap the other, ++
+            if (range1.isOverlappedBy(range2) || range2.isOverlappedBy(range1)) {
+                overlap++;
+            }
+        }
+        solution1 = contains;
+        solution2 = overlap;
     }
+
+    private Range getRange(String str) {
+        int borne1 = Integer.parseInt(str.split("-")[0]);
+        int borne2 = Integer.parseInt(str.split("-")[1]);
+        return new Range(borne1, borne2);
+    }
+
 }
