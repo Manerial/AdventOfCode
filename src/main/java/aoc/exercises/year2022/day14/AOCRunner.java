c41710edaae61ae2cd8acec6cac20eef55760d78
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:53:25 +0200

    Add exercise 14 of 2022

--- src/main/java/aoc/exercises/year2022/day14/AOCRunner.java
+++ src/main/java/aoc/exercises/year2022/day14/AOCRunner.java
@@ -1,6 +1,10 @@
 package aoc.exercises.year2022.day14;
 
 import utilities.AbstractAOC;
+import aoc.common_objects.Position;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * <pre>
@@ -11,7 +15,36 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        Cavern cavern = new Cavern(getDepth(inputList));
+        cavern.setDropPosition(new Position(500, 0));
+        cavern.fillWithAir();
+
+        addRocks(cavern, inputList);
+
+        cavern.dropSandUntilAbyss();
+        solution1 = cavern.countSand();
+
+        cavern.fillBottomWithRocks();
+        cavern.dropSandUntilFull();
+        solution2 = cavern.countSand();
+    }
+
+    private int getDepth(List<String> list) {
+        return list.stream().flatMap(
+                item -> Stream.of(item.split(" -> "))
+                        .map(coordinates -> coordinates.split(",")[1])
+                        .map(Integer::parseInt)
+        ).max(Integer::compareTo).orElse(0);
+    }
+
+    private void addRocks(Cavern cavern, List<String> list) {
+        for (String walls : list) {
+            String[] wallCoordinates = walls.split(" -> ");
+            for (int index = 0; index < wallCoordinates.length - 1; index++) {
+                Position beginWall = new Position(wallCoordinates[index], ",");
+                Position endWall = new Position(wallCoordinates[index + 1], ",");
+                cavern.addRocks(beginWall, endWall);
+            }
+        }
     }
 }
