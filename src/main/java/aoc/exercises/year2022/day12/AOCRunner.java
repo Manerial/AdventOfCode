521d39703cbde1a90a5a3e2d4c22a86d89ee8615
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:52:41 +0200

    Add exercise 12 of 2022

--- src/main/java/aoc/exercises/year2022/day12/AOCRunner.java
+++ src/main/java/aoc/exercises/year2022/day12/AOCRunner.java
@@ -1,6 +1,9 @@
 package aoc.exercises.year2022.day12;
 
 import utilities.AbstractAOC;
+import aoc.common_objects.Position;
+
+import java.util.List;
 
 /**
  * <pre>
@@ -9,9 +12,36 @@ import utilities.AbstractAOC;
  */
 public class AOCRunner extends AbstractAOC {
 
+    private final HeightMap grid = new HeightMap();
+
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        initiateGridGetLastCell(inputList);
+        grid.calculateGridFromStartAndEnd();
+        solution1 = grid.getLastCell().getDistanceFromStart();
+
+        solution2 = grid.getNearestCell().getDistanceFromEnd();
+    }
+
+    private void initiateGridGetLastCell(List<String> list) {
+        int y = 0;
+        for (String line : list) {
+            int x = 0;
+            for (char c : line.toCharArray()) {
+                Position position = new Position(x, y);
+                Cell cell = new Cell(position, c);
+                grid.addCell(position, cell);
+                if (c == 'S') {
+                    cell.setHeight('a');
+                    cell.setDistance(0, true);
+                }
+                if (c == 'E') {
+                    cell.setHeight('z');
+                    cell.setDistance(0, false);
+                }
+                x++;
+            }
+            y++;
+        }
     }
 }
