29c5e92e390cbe84d5a24e0c52666f5cb3c6eb7a
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:47:25 +0200

    Add exercise 02 of 2016

--- src/main/java/aoc/exercises/year2016/day02/AOCRunner.java
+++ src/main/java/aoc/exercises/year2016/day02/AOCRunner.java
@@ -2,6 +2,8 @@ package aoc.exercises.year2016.day02;
 
 import utilities.AbstractAOC;
 
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2016/day/2">here</a>
@@ -11,7 +13,30 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<String> directionsList = inputList;
+
+        Keypad keypad = new Keypad('5');
+        keypad.addKeyRow("123");
+        keypad.addKeyRow("456");
+        keypad.addKeyRow("789");
+        solution1 = getCode(directionsList, keypad);
+
+        keypad = new Keypad('5');
+        keypad.addKeyRow("  1  ");
+        keypad.addKeyRow(" 234 ");
+        keypad.addKeyRow("56789");
+        keypad.addKeyRow(" ABC ");
+        keypad.addKeyRow("  D  ");
+        solution2 = getCode(directionsList, keypad);
+    }
+
+    private String getCode(List<String> directionsList, Keypad keypad) {
+        StringBuilder code = new StringBuilder();
+        for (String directions : directionsList) {
+            directions.chars()
+                    .forEach(direction -> keypad.move((char) direction));
+            code.append(keypad.getCurrentKey());
+        }
+        return code.toString();
     }
 }
