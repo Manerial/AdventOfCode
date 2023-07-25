151aa83d4b391519745447ff8db1d7fc90353086
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 13:49:21 +0200

    Add exercise 06 of 2022

--- src/main/java/aoc/exercises/year2022/day06/AOCRunner.java
+++ src/main/java/aoc/exercises/year2022/day06/AOCRunner.java
@@ -11,7 +11,22 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        for (String item : inputList) {
+            solution1 = findFirstPacket(item, 4);
+            solution2 = findFirstPacket(item, 14);
+        }
+    }
+
+    private int findFirstPacket(String item, int size) {
+        int incr = 0;
+        while (incr < item.length() - size) {
+            String subStr = item.substring(incr, incr + size);
+            long count = subStr.chars().distinct().count();
+            if (count == size) {
+                return incr + size;
+            }
+            incr++;
+        }
+        return 0;
     }
 }
