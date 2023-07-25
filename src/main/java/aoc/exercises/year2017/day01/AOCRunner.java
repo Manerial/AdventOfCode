3e1a27e5ba5159d47865e1516c07d94718fe8407
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:17:53 +0200

    Add exercise 01 of 2017

--- src/main/java/aoc/exercises/year2017/day01/AOCRunner.java
+++ src/main/java/aoc/exercises/year2017/day01/AOCRunner.java
@@ -2,6 +2,9 @@ package aoc.exercises.year2017.day01;
 
 import utilities.AbstractAOC;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2017/day/1">here</a>
@@ -11,7 +14,27 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> list = inputList.get(0)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .map(Character::getNumericValue)
+                .toList();
+        List<Integer> list2 = new ArrayList<>(list.subList(1, list.size()));
+        list2.add(list.get(0));
+        solution1 = sumSameChars(list, list2);
+
+        List<Integer> subList1 = list.subList(0, list.size() / 2);
+        List<Integer> subList2 = list.subList(list.size() / 2, list.size());
+        solution2 = sumSameChars(subList1, subList2) * 2;
+    }
+
+    private static int sumSameChars(List<Integer> list1, List<Integer> list2) {
+        int sum = 0;
+        for (int index = 0; index < list1.size(); index++) {
+            if (list1.get(index).equals(list2.get(index))) {
+                sum += list1.get(index);
+            }
+        }
+        return sum;
     }
 }
