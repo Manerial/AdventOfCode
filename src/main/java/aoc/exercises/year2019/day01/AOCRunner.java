9fabe81560fdefe666f9cafed5c993b720587a9e
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:19:19 +0200

    Add exercise 01 of 2019

--- src/main/java/aoc/exercises/year2019/day01/AOCRunner.java
+++ src/main/java/aoc/exercises/year2019/day01/AOCRunner.java
@@ -2,6 +2,8 @@ package aoc.exercises.year2019.day01;
 
 import utilities.AbstractAOC;
 
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2019/day/1">here</a>
@@ -11,7 +13,35 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> masses = inputList.stream()
+                .map(Integer::parseInt)
+                .toList();
+        solution1 = sumList(getFuel(masses));
+
+        solution2 = operateRecursive(masses);
+    }
+
+    private int operateRecursive(List<Integer> masses) {
+        List<Integer> fuelNeeded = getFuel(masses);
+        int sum = sumList(fuelNeeded);
+        if (sum != 0) {
+            sum += operateRecursive(fuelNeeded);
+        }
+        return sum;
+    }
+
+    private int sumList(List<Integer> list) {
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private List<Integer> getFuel(List<Integer> masses) {
+        return masses.stream()
+                .map(integer -> {
+                    int sum = integer / 3 - 2;
+                    return Math.max(sum, 0);
+                })
+                .toList();
     }
 }
