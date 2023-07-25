8bf557b4bac7c91d5db753d7f25373fd5373bb4d
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:18:03 +0200

    Add exercise 01 of 2018

--- src/main/java/aoc/exercises/year2018/day01/AOCRunner.java
+++ src/main/java/aoc/exercises/year2018/day01/AOCRunner.java
@@ -2,6 +2,10 @@ package aoc.exercises.year2018.day01;
 
 import utilities.AbstractAOC;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2018/day/1">here</a>
@@ -11,7 +15,27 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        List<Integer> frequencyDeltaList = inputList.stream()
+                .map(Integer::parseInt)
+                .toList();
+
+        solution1 = frequencyDeltaList.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        solution2 = getFirstFrequencyFetchedTwice(frequencyDeltaList);
+    }
+
+    private static int getFirstFrequencyFetchedTwice(List<Integer> frequencyDeltaList) {
+        int currentFrequency = 0;
+        Set<Integer> alreadyFetched = new HashSet<>(Set.of(currentFrequency));
+        while (true) {
+            for (Integer frequencyDelta : frequencyDeltaList) {
+                currentFrequency += frequencyDelta;
+                if (!alreadyFetched.add(currentFrequency)) {
+                    return currentFrequency;
+                }
+            }
+        }
     }
 }
