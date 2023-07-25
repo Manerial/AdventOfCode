83048cdff2923c98067d19bbc6a2ec4bdf8ec08d
Manerial <avr1l.dunmare@gmail.com>
2023-07-25 10:32:57 +0200

    Add exercise 04 of 2015

--- src/main/java/aoc/exercises/year2015/day04/AOCRunner.java
+++ src/main/java/aoc/exercises/year2015/day04/AOCRunner.java
@@ -1,5 +1,6 @@
 package aoc.exercises.year2015.day04;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import utilities.AbstractAOC;
 
 /**
@@ -7,11 +8,30 @@ import utilities.AbstractAOC;
  * Instructions are <a href="https://adventofcode.com/2015/day/4">here</a>
  * </pre>
  */
+// Can't be optimized
 public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        String input = inputList.get(0);
+        solution1 = findPasswordForLeading(input, "00000");
+        solution2 = findPasswordForLeading(input, "000000");
+    }
+
+    /**
+     * Test all combinations of input + password to find the first one for which the hash starts with the given leading.
+     *
+     * @param input   : the input to use
+     * @param leading : the first characters to find in the hash
+     * @return the first password that matches the conditions
+     */
+    private static int findPasswordForLeading(String input, String leading) {
+        int password = 0;
+        String output = "";
+        while (!output.startsWith(leading)) {
+            password++;
+            output = DigestUtils.md5Hex(input + password);
+        }
+        return password;
     }
 }
