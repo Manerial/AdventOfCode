5f277163c213411ca511339f5f2341ff91bbb661
Manerial <avr1l.dunmare@gmail.com>
2023-08-01 17:44:17 +0200

    Add exercise 07 of 2021

--- src/main/java/aoc/exercises/year2021/day07/AOCRunner.java
+++ src/main/java/aoc/exercises/year2021/day07/AOCRunner.java
@@ -2,6 +2,8 @@ package aoc.exercises.year2021.day07;
 
 import utilities.AbstractAOC;
 
+import java.util.Arrays;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2021/day/7">here</a>
@@ -11,7 +13,44 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        int[] crabsPosition = Arrays.stream(inputList.get(0).split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        solution1 = getFuelToAlign(crabsPosition, false);
+
+        solution2 = getFuelToAlign(crabsPosition, true);
+    }
+
+    private int getFuelToAlign(int[] crabsPosition, boolean expensiveSolution) {
+        int minPosition = Arrays.stream(crabsPosition).min().orElseThrow();
+        int maxPosition = Arrays.stream(crabsPosition).max().orElseThrow();
+        int minFuel = Integer.MAX_VALUE;
+        for (int position = minPosition; position < maxPosition; position++) {
+            int currentFuel = getFuelForPosition(crabsPosition, position, expensiveSolution);
+            minFuel = Integer.min(minFuel, currentFuel);
+        }
+        return minFuel;
+    }
+
+    private int getFuelForPosition(int[] crabsPosition, int position, boolean expensiveSolution) {
+        return Arrays.stream(crabsPosition)
+                .map(operand -> {
+                    int move = Math.abs(operand - position);
+                    if (!expensiveSolution) {
+                        return move;
+                    } else {
+                        return getPascalNumber(move);
+                    }
+                })
+                .sum();
+    }
+
+    private int getPascalNumber(int move) {
+        int pascalNumber = 0;
+        for(int i = 0; i <= move; i++) {
+            pascalNumber += i;
+        }
+        return pascalNumber;
     }
 }
