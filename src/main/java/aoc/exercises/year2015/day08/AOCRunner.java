27d2b83bd39bf347c63f39988335e19783116813
Manerial <avr1l.dunmare@gmail.com>
2023-08-06 10:11:17 +0200

    Add exercise 08 of 2015

--- src/main/java/aoc/exercises/year2015/day08/AOCRunner.java
+++ src/main/java/aoc/exercises/year2015/day08/AOCRunner.java
@@ -11,7 +11,45 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        int totalLength = 0;
+        int decodedLength = 0;
+        int encodedLength = 0;
+        for (String line : inputList) {
+            totalLength += line.length();
+            decodedLength += getDecodedLength(line);
+            encodedLength += getEncodedLength(line);
+        }
+
+        solution1 = totalLength - decodedLength; // Deleted characters
+        solution2 = encodedLength - totalLength; // Inserted characters
+    }
+
+    /**
+     * Decode the string and return its length:
+     * "aaa\"aaa" -> aaa"aaa
+     *
+     * @param line : the string to decode
+     * @return the decoded string length
+     */
+    private int getDecodedLength(String line) {
+        return line
+                .replace("\\\"", "\"") // \" -> "
+                .replace("\\\\", "\\") // \\ -> \
+                .replaceAll("\\\\x[0-9a-f]{2}", "0") // \\x2f -> 0
+                .length() - 2;
+    }
+
+    /**
+     * Over encode the string and return its length :
+     * "aaa\"aaa" -> "\"aaa\\\"aaa\""
+     *
+     * @param line : the string to encode
+     * @return the encoded string length
+     */
+    private int getEncodedLength(String line) {
+        return line
+                .replace("\\", "\\\\") // \ -> \\
+                .replace("\"", "\\\"") // " -> \"
+                .length() + 2;
     }
 }
--- src/test/java/aoc/exercises/year2015/day08/AOCRunnerTest.java
+++ src/test/java/aoc/exercises/year2015/day08/AOCRunnerTest.java
@@ -16,15 +16,15 @@ class AOCRunnerTest {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), true);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(12), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(19), aocRunner.getSolution2().toString()));
     }
     @Test
     void test_puzzle() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), false);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(1371), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(2117), aocRunner.getSolution2().toString()));
     }
 }
