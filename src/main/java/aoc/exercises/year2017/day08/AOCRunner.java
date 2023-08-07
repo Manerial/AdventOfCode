4f1d650e58239d0b54462c6db27ea7982edc7b66
Manerial <avr1l.dunmare@gmail.com>
2023-08-08 01:07:35 +0200

    Add exercise 08 of 2017

--- src/main/java/aoc/exercises/year2017/day08/AOCRunner.java
+++ src/main/java/aoc/exercises/year2017/day08/AOCRunner.java
@@ -2,16 +2,86 @@ package aoc.exercises.year2017.day08;
 
 import utilities.AbstractAOC;
 
+import java.util.Map;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2017/day/8">here</a>
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    private Map<String, Integer> registers;
+    private int maxValue = 0;
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        InputParser inputParser = new InputParser(inputList);
+        registers = inputParser.parseInput();
+        compute();
+        solution1 = registers.values().stream().mapToInt(Integer::intValue).max().orElseThrow();
+        solution2 = maxValue;
+    }
+
+    private void compute() {
+        for (String command : inputList) {
+            String[] splitLine = command.split(" ");
+            String register = splitLine[0];
+            String operation = splitLine[1];
+            int operand = Integer.parseInt(splitLine[2]);
+            String condRegister = splitLine[4];
+            String condition = splitLine[5];
+            int condValue = Integer.parseInt(splitLine[6]);
+
+            if (operation.equals("dec")) {
+                operand = -operand;
+            }
+
+            int newValue = getRegisterValue(register, operand, condRegister, condition, condValue);
+
+            registers.put(register, newValue);
+        }
+    }
+
+    private int getRegisterValue(String register, int operand, String condRegister, String condition, int condValue) {
+        int newValue = getRegister(register);
+        switch (condition) {
+            case ">" -> {
+                if (getRegister(condRegister) > condValue) {
+                    newValue += operand;
+                }
+            }
+            case "<" -> {
+                if (getRegister(condRegister) < condValue) {
+                    newValue += operand;
+                }
+            }
+            case ">=" -> {
+                if (getRegister(condRegister) >= condValue) {
+                    newValue += operand;
+                }
+            }
+            case "<=" -> {
+                if (getRegister(condRegister) <= condValue) {
+                    newValue += operand;
+                }
+            }
+            case "!=" -> {
+                if (getRegister(condRegister) != condValue) {
+                    newValue += operand;
+                }
+            }
+            case "==" -> {
+                if (getRegister(condRegister) == condValue) {
+                    newValue += operand;
+                }
+            }
+            default -> throw new UnsupportedOperationException(condition + " is not a valid condition");
+        }
+        maxValue = Math.max(maxValue, newValue);
+        return newValue;
+    }
+
+    private int getRegister(String register) {
+        return registers.get(register);
     }
 }
