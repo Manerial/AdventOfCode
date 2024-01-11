816b8e3a0db62fe1e1838f19c7f43d2d8f2877f1
Manerial <avr1l.dunmare@gmail.com>
2024-01-11 17:15:26 +0100

    Add exercise 04 of 2023

--- src/main/java/aoc/exercises/year2023/day04/AOCRunner.java
+++ src/main/java/aoc/exercises/year2023/day04/AOCRunner.java
@@ -2,6 +2,8 @@ package aoc.exercises.year2023.day04;
 
 import utilities.AbstractAOC;
 
+import java.util.Map;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2023/day/4">here</a>
@@ -11,7 +13,26 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        InputParser inputParser = new InputParser(inputList);
+        Map<Integer, Card> cardsById = inputParser.parseInput();
+        cardsById.values().forEach(card -> scratchCard(card, cardsById));
+
+        solution1 = cardsById.values().stream()
+                .mapToInt(Card::getPoints)
+                .sum();
+        solution2 = cardsById.values().stream()
+                .mapToInt(Card::getStack)
+                .sum();
+    }
+
+    private static void scratchCard(Card card, Map<Integer, Card> cardsById) {
+        int winPoints = card.countWinNumbers();
+        for (int i = 1; i <= winPoints; i++) {
+            int nextCardId = card.getId() + i;
+            cardsById.computeIfPresent(nextCardId, (id, nextCard) -> {
+                nextCard.addToStack(card.getStack());
+                return nextCard;
+            });
+        }
     }
 }
