3849ef3ecf31732150f313c24f37cac42b8ade93
Manerial <avr1l.dunmare@gmail.com>
2024-01-13 00:52:33 +0100

    Add exercise 05 of 2023

--- src/main/java/aoc/exercises/year2023/day05/AOCRunner.java
+++ src/main/java/aoc/exercises/year2023/day05/AOCRunner.java
@@ -1,5 +1,6 @@
 package aoc.exercises.year2023.day05;
 
+import org.apache.commons.lang3.tuple.Pair;
 import utilities.AbstractAOC;
 
 /**
@@ -11,7 +12,25 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        InputParser inputParser = new InputParser(inputList);
+        Almanach almanach = inputParser.parseInput();
+
+        solution1 = almanach.seeds().stream()
+                .mapToLong(value -> almanach.getSeedLocation(value).getLeft())
+                .min()
+                .orElse(0);
+
+        long min = Long.MAX_VALUE;
+        for (int seedIndex = 0; seedIndex < almanach.seeds().size(); seedIndex += 2) {
+            long seedStart = almanach.seeds().get(seedIndex);
+            long range = almanach.seeds().get(seedIndex + 1);
+            long nextRange;
+            for (long seed = seedStart; seed < seedStart + range; seed += nextRange) {
+                Pair<Long, Long> value = almanach.getSeedLocation(seed);
+                min = Math.min(min, value.getLeft());
+                nextRange = Math.max(1, value.getRight());
+            }
+        }
+        solution2 = min;
     }
 }
