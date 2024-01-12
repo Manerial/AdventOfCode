f1415fff4f9fb20984ce94c117cb4e95680d7969
Manerial <avr1l.dunmare@gmail.com>
2024-01-12 12:41:52 +0100

    Add year 2023

---
+++ src/main/java/aoc/exercises/year2023/day15/AOCRunner.java
@@ -0,0 +1,17 @@
+package aoc.exercises.year2023.day15;
+
+import utilities.AbstractAOC;
+
+/**
+ * <pre>
+ * Instructions are <a href="https://adventofcode.com/2023/day/15">here</a>
+ * </pre>
+ */
+public class AOCRunner extends AbstractAOC {
+
+    @Override
+    public void run() {
+        solution1 = 0;
+        solution2 = 0;
+    }
+}
