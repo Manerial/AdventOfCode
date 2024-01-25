d0c7d06400a6a9bcfc46b4a3c1915110ecfc78ba
Manerial <avr1l.dunmare@gmail.com>
2024-01-25 11:58:33 +0100

    Add exercise 23 of 2022

--- src/main/java/aoc/exercises/year2022/day23/AOCRunner.java
+++ src/main/java/aoc/exercises/year2022/day23/AOCRunner.java
@@ -1,6 +1,7 @@
 package aoc.exercises.year2022.day23;
 
 import utilities.AbstractAOC;
+import utilities.Timer;
 
 /**
  * <pre>
@@ -11,7 +12,20 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        Timer timer = new Timer();
+        InputParser inputParser = new InputParser(inputList);
+        Grid grid = inputParser.parseInput();
+        int rounds = 0;
+        for (; rounds < 10; rounds++) {
+            grid.move();
+        }
+        solution1 = grid.getTotalArea();
+        timer.time();
+        while (!grid.finalized()) {
+            grid.move();
+            rounds++;
+        }
+        solution2 = rounds;
+        timer.time();
     }
 }
