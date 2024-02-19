c9c10ff2433134cb44f29e9ab9ced3ada1698c32
Manerial <avr1l.dunmare@gmail.com>
2024-02-19 16:35:34 +0100

    Add exercise 19 of 2022

--- src/main/java/aoc/exercises/year2022/day19/AOCRunner.java
+++ src/main/java/aoc/exercises/year2022/day19/AOCRunner.java
@@ -2,16 +2,32 @@ package aoc.exercises.year2022.day19;
 
 import utilities.AbstractAOC;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2022/day/19">here</a>
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    List<RobotFactory> robotFactories = new ArrayList<>();
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        for (String blueprint : inputList) {
+            RobotFactory robotFactory = new RobotFactory(blueprint);
+            robotFactories.add(robotFactory);
+        }
+        robotFactories.forEach(robotFactory -> robotFactory.runFor(24));
+        solution1 = robotFactories.stream().mapToInt(RobotFactory::getBlueprintQuality).sum();
+
+        robotFactories.stream()
+                .limit(3)
+                .forEach(robotFactory -> robotFactory.runFor(32));
+        solution2 = robotFactories.stream().map(RobotFactory::getMaxGeodes)
+                .limit(3)
+                .reduce((left, right) -> left * right)
+                .orElse(0);
     }
 }
