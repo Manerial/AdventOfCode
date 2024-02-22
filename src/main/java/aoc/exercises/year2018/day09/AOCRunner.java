6d8928dd73345dd0fdc2f6fd218d75f81c4c60b3
Manerial <avr1l.dunmare@gmail.com>
2024-02-22 14:34:18 +0100

    Add exercise 09 of 2018

--- src/main/java/aoc/exercises/year2018/day09/AOCRunner.java
+++ src/main/java/aoc/exercises/year2018/day09/AOCRunner.java
@@ -1,6 +1,9 @@
 package aoc.exercises.year2018.day09;
 
+import aoc.common_objects.RoundDeque;
 import utilities.AbstractAOC;
+
+import java.util.Arrays;
 
 /**
  * <pre>
@@ -8,10 +11,48 @@ import utilities.AbstractAOC;
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    private RoundDeque<Integer> marbles;
+    private int players;
+    private int lastMarble;
+    private long[] playersScore;
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        initGame();
+        play();
+        solution1 = Arrays.stream(playersScore).max().orElseThrow();
+        lastMarble *= 100;
+        reinitGame();
+        play();
+        solution2 = Arrays.stream(playersScore).max().orElseThrow();
+    }
+
+    private void initGame() {
+        String[] input = inputList.get(0)
+                .replace(" points", "")
+                .split(" players; last marble is worth ");
+        players = Integer.parseInt(input[0]);
+        lastMarble = Integer.parseInt(input[1]);
+        reinitGame();
+    }
+
+    private void reinitGame() {
+        marbles = new RoundDeque<>();
+        playersScore = new long[players];
+        marbles.add(0);
+    }
+
+
+    private void play() {
+        for (int i = 1; i <= lastMarble; i++) {
+            if (i % 23 == 0) {
+                marbles.rotate(-7);
+                int player = i % players;
+                playersScore[player] += i + marbles.removeFirst();
+            } else {
+                marbles.rotate(2);
+                marbles.addFirst(i);
+            }
+        }
     }
 }
