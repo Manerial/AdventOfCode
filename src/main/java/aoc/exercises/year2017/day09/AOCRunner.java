899ebf0a86fc26a2788b7ba7685d6cb9623997d0
Manerial <avr1l.dunmare@gmail.com>
2024-02-22 12:07:53 +0100

    Add exercise 09 of 2017

--- src/main/java/aoc/exercises/year2017/day09/AOCRunner.java
+++ src/main/java/aoc/exercises/year2017/day09/AOCRunner.java
@@ -11,7 +11,37 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        String line = inputList.get(0);
+        String cleanCancel = cleanCancelCharacters(line);
+        long numberOfGarbageChars = countGarbage(cleanCancel) * 2;
+        String fullyClean = cleanGarbage(cleanCancel);
+        solution1 = countGroups(fullyClean);
+        solution2 = cleanCancel.length() - fullyClean.length() - numberOfGarbageChars;
+    }
+
+    private long countGarbage(String line) {
+        return line.chars().filter(c -> c == '>').count();
+    }
+
+    private String cleanCancelCharacters(String line) {
+        return line.replaceAll("!.", "");
+    }
+
+    private String cleanGarbage(String line) {
+        return line.replaceAll("<[^>]*>", "");
+    }
+
+    private int countGroups(String line) {
+        int count = 0;
+        int groupIndex = 1;
+        for (char c : line.toCharArray()) {
+            if (c == '{') {
+                count += groupIndex;
+                groupIndex++;
+            } else if (c == '}') {
+                groupIndex--;
+            }
+        }
+        return count;
     }
 }
