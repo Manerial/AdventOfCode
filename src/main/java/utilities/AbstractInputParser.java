7617d2b51340c24910a0c75eda08bee25230da93
Manerial <avr1l.dunmare@gmail.com>
2024-02-27 09:28:15 +0100

    Update AbstractInputParser to add default ParseInput

--- src/main/java/utilities/AbstractInputParser.java
+++ src/main/java/utilities/AbstractInputParser.java
@@ -1,11 +1,17 @@
 package utilities;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 import java.util.List;
 
-public abstract class AbstractInputParser {
+public abstract class AbstractInputParser<T> {
     protected List<String> inputList;
 
     protected AbstractInputParser(List<String> inputList) {
         this.inputList = inputList;
+    }
+
+    public T parseInput() {
+        throw new NotImplementedException();
     }
 }
