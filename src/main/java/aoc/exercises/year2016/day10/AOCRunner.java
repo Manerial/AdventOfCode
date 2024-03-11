7e588c1936abbab8772ecce7516ecdf55025ae36
Manerial <avr1l.dunmare@gmail.com>
2024-03-11 17:18:00 +0100

    Add exercise 10 of 2016

--- src/main/java/aoc/exercises/year2016/day10/AOCRunner.java
+++ src/main/java/aoc/exercises/year2016/day10/AOCRunner.java
@@ -1,6 +1,12 @@
 package aoc.exercises.year2016.day10;
 
+import org.apache.commons.lang3.tuple.Pair;
 import utilities.AbstractAOC;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <pre>
@@ -8,10 +14,51 @@ import utilities.AbstractAOC;
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    Map<Integer, Bot> bots = new HashMap<>();
+    Map<Integer, Integer> outputs = new HashMap<>();
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        InputParser inputParser = new InputParser(inputList);
+        bots = inputParser.parseInput();
+
+        playBotsInstructions();
+
+        if (isExample) {
+            solution1 = 0;
+        } else {
+            solution1 = bots.values().stream()
+                    .filter(bot -> bot.hasCompared("1761"))
+                    .findAny()
+                    .orElseThrow()
+                    .getId();
+        }
+        solution2 = outputs.get(0) * outputs.get(1) * outputs.get(2);
+    }
+
+    /**
+     * Each bot that has two chips will play its instructions and distribute the chips to its planned destinations (bot / output).
+     */
+    private void playBotsInstructions() {
+        Bot firstBot = bots.values().stream()
+                .filter(Bot::hasTwoChips)
+                .findAny()
+                .orElse(null);
+
+        Deque<Bot> botsWithTwoChips = new ArrayDeque<>();
+        botsWithTwoChips.add(firstBot);
+
+        while (!botsWithTwoChips.isEmpty()) {
+            Bot currentBot = botsWithTwoChips.pop();
+            Pair<Bot, Bot> lowAndHigh = currentBot.compareAndGive(bots, outputs);
+            Bot lowBot = lowAndHigh.getLeft();
+            Bot highBot = lowAndHigh.getRight();
+            if (lowBot != null && lowBot.hasTwoChips()) {
+                botsWithTwoChips.add(lowBot);
+            }
+            if (highBot != null && highBot.hasTwoChips()) {
+                botsWithTwoChips.add(highBot);
+            }
+        }
     }
 }
