c6fd11a2dfa3b698f6253d3fb27e21ca8c6e5f58
Manerial <avr1l.dunmare@gmail.com>
2024-12-03 11:46:01 +0100

    Add exercise 01 of 2024

--- src/test/java/aoc/exercises/year2024/day01/AOCRunnerTest.java
+++ src/test/java/aoc/exercises/year2024/day01/AOCRunnerTest.java
@@ -16,8 +16,8 @@ class AOCRunnerTest {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), true);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(11), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(31), aocRunner.getSolution2().toString()));
     }
 
     @Test
@@ -25,7 +25,7 @@ class AOCRunnerTest {
         AbstractAOC aocRunner = AOCRunnerTestBuilder.getAOCRunner(this.getClass(), false);
         aocRunner.run();
         assertAll(
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution1().toString()),
-                () -> assertEquals(String.valueOf(0), aocRunner.getSolution2().toString()));
+                () -> assertEquals(String.valueOf(1530215), aocRunner.getSolution1().toString()),
+                () -> assertEquals(String.valueOf(26800609), aocRunner.getSolution2().toString()));
     }
 }
