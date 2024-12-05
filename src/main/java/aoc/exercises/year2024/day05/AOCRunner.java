9b4b268f479ef838c959ccc2c1ed31e0fae20291
Manerial <avr1l.dunmare@gmail.com>
2024-12-05 11:35:06 +0100

    Add exercise 05 of 2024

--- src/main/java/aoc/exercises/year2024/day05/AOCRunner.java
+++ src/main/java/aoc/exercises/year2024/day05/AOCRunner.java
@@ -11,7 +11,14 @@ public class AOCRunner extends AbstractAOC {
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        InputParser inputParser = new InputParser(inputList);
+        PageUpdater pageUpdater = inputParser.parseInput();
+        solution1 = pageUpdater.getMatchingUpdates().stream()
+                .mapToInt(PageUpdate::getMiddlePage)
+                .sum();
+
+        solution2 = pageUpdater.getNonMatchingUpdatesCorrected().stream()
+                .mapToInt(PageUpdate::getMiddlePage)
+                .sum();
     }
 }
