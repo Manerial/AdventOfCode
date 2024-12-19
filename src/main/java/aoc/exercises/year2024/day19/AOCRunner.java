463f63af32d0af34b757ea558bbdd2b9d713d64e
Manerial <avr1l.dunmare@gmail.com>
2024-12-19 16:59:37 +0100

    Add exercise 19 of 2024

--- src/main/java/aoc/exercises/year2024/day19/AOCRunner.java
+++ src/main/java/aoc/exercises/year2024/day19/AOCRunner.java
@@ -2,16 +2,57 @@ package aoc.exercises.year2024.day19;
 
 import utilities.AbstractAOC;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * <pre>
  * Instructions are <a href="https://adventofcode.com/2024/day/19">here</a>
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
+    private List<String> availableTowels;
+    private final Map<String, Long> waysToDo = new HashMap<>();
+    private int minDesignSize;
+    private int maxDesignSize;
 
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        availableTowels = List.of(inputList.get(0).split(", "));
+        minDesignSize = availableTowels.stream().mapToInt(String::length).min().orElseThrow();
+        maxDesignSize = availableTowels.stream().mapToInt(String::length).max().orElseThrow();
+        List<String> desiredDesigns = inputList.stream().skip(2).toList();
+
+        List<Long> waysToDoEachDesign = desiredDesigns.stream()
+                .map(this::computeWaysToDo)
+                .toList();
+
+        solution1 = waysToDoEachDesign.stream()
+                .filter(ways -> ways > 0)
+                .count();
+
+        solution2 = waysToDoEachDesign.stream()
+                .mapToLong(Long::longValue)
+                .sum();
+    }
+
+    private long computeWaysToDo(String design) {
+        if (design.isEmpty()) {
+            return 1;
+        } else if (waysToDo.containsKey(design)) {
+            return waysToDo.get(design);
+        }
+        long countDesigns = 0;
+        int maxSizeCurrentTowel = Math.min(maxDesignSize, design.length());
+        for (int size = minDesignSize; size <= maxSizeCurrentTowel; size++) {
+            String currentTowel = design.substring(0, size);
+            if (availableTowels.contains(currentTowel)) {
+                String nextDesign = design.substring(size);
+                countDesigns += computeWaysToDo(nextDesign);
+            }
+        }
+        waysToDo.put(design, countDesigns);
+        return countDesigns;
     }
 }
