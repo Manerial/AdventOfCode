0683e5e4d5a16aa09f686ef6e92ebc80390aa2bd
Manerial <avr1l.dunmare@gmail.com>
2025-12-04 11:09:09 +0100

    Add exercise 04 of 2025

--- src/main/java/aoc/exercises/year2025/day04/AOCRunner.java
+++ src/main/java/aoc/exercises/year2025/day04/AOCRunner.java
@@ -1,6 +1,9 @@
 package aoc.exercises.year2025.day04;
 
+import aoc.common_objects.*;
 import utilities.*;
+
+import java.util.*;
 
 /**
  * <pre>
@@ -8,10 +11,43 @@ import utilities.*;
  * </pre>
  */
 public class AOCRunner extends AbstractAOC {
-
     @Override
     public void run() {
-        solution1 = 0;
-        solution2 = 0;
+        InputParser inputParser = new InputParser(inputList);
+        Map<Position, Roll> rolls = inputParser.parseInput();
+        long initialSize = rolls.size();
+
+        removeRolls(rolls);
+        solution1 = initialSize - rolls.size();
+
+        removeRollsUntilEnd(rolls);
+        solution2 = initialSize - rolls.size();
+    }
+
+    /**
+     * Remove the accessible rolls and all their occurrences in their neighbors
+     *
+     * @param rolls The map of rolls
+     */
+    private void removeRolls(Map<Position, Roll> rolls) {
+        List<Roll> accessibleRolls = rolls.values().stream()
+                .filter(Roll::isAccessible)
+                .toList();
+
+        accessibleRolls.forEach(roll -> roll.removeAllOccurrences(rolls));
+    }
+
+    /**
+     * Remove the accessible rolls until no more can be removed
+     *
+     * @param rolls The map of rolls
+     */
+    private void removeRollsUntilEnd(Map<Position, Roll> rolls) {
+        long currentRollSize;
+        do {
+            currentRollSize = rolls.size();
+            removeRolls(rolls);
+        } while (currentRollSize != rolls.size());
+        removeRolls(rolls);
     }
 }
